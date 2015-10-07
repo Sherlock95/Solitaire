325f8cd1447fcec78363a7604a56f0b740d61c97
Sherlock <jarrettergolocke@gmail.com>
2015-10-07 09:50:18 -0500

    Committing minor changes

--- Solitaire.js
+++ Solitaire.js
@@ -1,80 +1,125 @@
-var canvas;
-var gl;
-
-var vPosition;
-var vColor;
-
-// Empyt array/matrix that will hold all the cards in the game.
-var cards = [];
-var positions1 = [
-    vec4( -0.5, 0.5 )
-];
-var positions2 = [
-    vec4( 0.5, 0.5 )
-];
-var positions3 = [
-    vec4( -0.5, -0.5 )
-];
-var positions4 = [
-    vec4( 0.5, -0.5 )
-];
-
 window.onload = function init()
 {
+    var canvas;
+    var gl;
+
+    var vPosition;
+    var vColor;
+    var program;
+
+    // Empyt array/matrix that will hold all the cards in the game.
+    var cards = new Array( 52 );
+    var textures = new Array( 53 );
+    var positionsX = new Array( 52 * 4 );
+    var positionsY = new Array( 52 * 4 );
+    var positionsZ = new Array( 52 * 4 );
+
     canvas = document.getElementById( "gl-canvas" );
-    
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    //
-    //  Configure WebGL
-    //
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.165, 0.714, 0.043, 1.0 );
-    
-    //  Load shaders and initialize attribute buffers
-    
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
-    
-    // Load the data into the GPU
-    
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(cards), gl.STATIC_DRAW );
+    var gl = initWebGL( canvas );
 
-    // Associate out shader variables with our data buffer
-    
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
+    if ( gl ) {
+        gl.clearColor( 0.165, 0.714, 0.043, 1.0 );
+        gl.enable( DEPTH_TEST );
+        gl.clear( gl.COLOR_BUFFER_BIT );
+        gl.viewport( 0, 0, canvas.width, canvas.height );
+    }
 
-    var vColor = gl.getAttribLocation( program, "vColor" );
-    gl.vertexAttribPointer( vColor, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vColor );
-
-    initCards();
+    initShaders();
 
     render();
+
+    function render() 
+    {
+    }
+
+    function initWebGL( canvas ) 
+    {
+        gl = null;
+
+        gl = canvas.getContext( "webgl" );
+
+        if ( !gl ) 
+        {
+            alert( "Unable to initialize webgl." );
+        }
+
+        return gl;
+    }
+
+    function initShaders()
+    {
+        var vertexShader = getShader( gl, "vertex-shader" );
+        var fragmentShader = getShader( gl, "fragment-shader" );
+
+        shaderProgram = gl.createProgram();
+        gl.attachShader( shaderProgram, vertexShader );
+        gl.attachShader( shaderProgram, fragmentShader );
+        gl.linkProgram( shaderProgram );
+        
+        if ( !gl.getProgramParameter( shaderProgram, gl.LINK_STATUS ) )
+        {
+            alert( "Unable to initialize the shader program." );
+        }
+
+        gl.useProgram( shaderProgram );
+
+        vPosition = gl.getAttribLocation( shaderProgram, "vPosition" );
+        gl.enableVertexAttribArray( vPosition );
+        vColor = gl.getAttribLocation( shaderProgram, "vColor" );
+        gl.enableVertexAttribArray( vColor );
+        u_resolution = gl.getAttribLocation( shaderProgram, "u_resolution" );
+        gl.enableVertexAttribArray( u_resolution );
+    }
+
+    function getShader( gl, id )
+    {
+        var shaderScript, theSource, currentChild, shader;
+
+        shaderScript = document.getElementById( id );
+
+        if ( !shaderScript )
+        {
+            return null;
+        }
+
+        theSource = "";
+        currentChild = shaderScript.firstChild;
+
+        while ( currentChild )
+        {
+            if ( currentChild.nodeType == currentChild.TEXT_NODE )
+            {
+                theSource += currentChild.textContent;
+            }
+
+            currentChild = currentChild.nextSibling;
+        }
+
+        if ( shaderScript.type == "x-shader/x-fragment" )
+        {
+            shader = gl.createShader( gl.FRAGMENT_SHADER );
+        }
+        else if ( shaderScript.type == "x-shader/x-vertex" )
+        {
+            shader = gl.createShader( gl.VERTEX_SHADER );
+        }
+        else
+        {
+            return null;
+        }
+
+        gl.shaderSource( shader, theSource );
+
+        gl.compileShader( shader );
+
+        if ( !gl.getShaderParameter( shader, gl.COMPILE_STATUS ) )
+        {
+            alert( "An error occurred while compiling shaders: " + gl.getShaderInfoLog( shader ) );
+            return null;
+        }
+
+        return shader
+    }
 };
 
-function initCards()
-{
-    var cardTexture = gl.createTexture();
-    cardImage = new Image();
-    cardImage.onLoad = function() { handleTextureLoading( cardImage, cardTexture ); };
-    cardImage.src = "img/acespades.png";
-    cards[ 0 ] = {
-        card: 1,
-        texture: cardTexture,
-        image: cardImage,
-        color: 0,
-        vertices: vec4( positions1[0], positions2[0], positions3[0], positions4[0] )
-    };
-}
-
-
-function render() {
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.POINTS, 0, cards.length );
-}
