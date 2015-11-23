526a94a7ce422b36b36e4a120974bdbef537c080
Sherlock <jarrettergolocke@gmail.com>
2015-11-23 15:53:37 -0600

    Moved some things around to make it more organized and easier to understand. Hopefully DOD friendly.

--- Solitaire.js
+++ Solitaire.js
@@ -40,23 +40,175 @@ var selected = false;
 
 var oldX, oldY;
 
-var CARD_WIDTH = 0.15;
-var CARD_HEIGHT = 0.4;
-var offset = 0.15;
-
 var deck = []; // holds index for all cards.
-var card_x = []; // holds x values for all cards
-var card_y = []; // holds y values for all cards
-var stacks = [ [], [], [], [], [], [], 
-               [], [], [], [], [], [] ]; // holds the index of cards in stacks.
-var stack_pos = []; // holds positions of the stacks.
-var render_card = []; // holds index of cards being rendered
-var render_vertices = []; // holds the position of cards being rendered
-var face_up = []; // tells you which cards that are being rendered are face up
-var place_pos = []; // tells which positions are legal for placement.
-var click_pos = []; // tells which positions can be clicked.
 
-window.onload = function init()
+var card_dim = {
+    width: 0.15,
+    height: 0.4,
+    offset: 0.15
+};
+
+var cards = {
+    cards_x: [], // holds x values for all cards
+    cards_y: []  // holds y values for all cards
+};
+
+var card_info = {
+    face_up: [],   // tells you which cards that are being rendered are face up
+    place_pos: [], // tells which positions are legal for placement.
+    click_pos: []  // tells which positions can be clicked.
+};
+
+var stack_info = {
+    stacks: [ [], [], [], [], [], [], 
+              [], [], [], [], [], [] ], // holds the index of cards in stacks.
+    stack_pos: [] // holds positions of the stacks.
+};
+
+var render_info = {
+    render_vertices_x: [], // holds the x position of cards being rendered
+    render_vertices_y: [], // holds the y position of cards being rendered
+    render_colors: []    // holds the colors of all cards.
+};
+
+function render()
+{
+    gl.clear( gl.COLOR_BUFFER_BIT );
+
+    // Push card positions to vertex shader buffer.
+    for ( var i = 0; i < render_info.render_vertices_x.length; ++i )
+    {
+        gl.bindBuffer( gl.ARRAY_BUFFER, verticesBuffer );
+        var tmp = [
+            vec2( render_info.render_vertices_x[ i ], 
+                  render_info.render_vertices_y[ i ] ),
+            vec2( render_info.render_vertices_x[ i ] + card_dim.width, 
+                  render_info.render_vertices_y[ i ] ),
+            vec2( render_info.render_vertices_x[ i ], 
+                  render_info.render_vertices_y[ i ] - card_dim.height ),
+            vec2( render_info.render_vertices_x[ i ] + card_dim.width,
+                  render_info.render_vertices_y[ i ] - card_dim.height ),
+            vec2( render_info.render_vertices_x[ i ], 
+                  render_info.render_vertices_y[ i ] - card_dim.height ),
+            vec2( render_info.render_vertices_x[ i ] + card_dim.width, 
+                  render_info.render_vertices_y[ i ] )
+        ];
+        gl.bufferSubData( gl.ARRAY_BUFFER, 48 * i, flatten( tmp ) );
+    }
+
+    gl.drawArrays( gl.TRIANGLES, 0, 312 );
+}
+
+function mouseDown( e )
+{
+    // Tell the program that the mouse is pressing down.
+    drag = true;
+    
+    // Get current position of the mouse in terms of WebGL.
+    var currX = 2 * e.clientX / canvas.width - 1;
+    var currY = 2 * ( canvas.height - e.clientY ) / canvas.height - 1;
+}
+
+//NOTE: 0 - 6 are field stacks, 7 is draw deck, 8 - 11 are the finish stacks
+function mouseUp( e )
+{
+    drag = false;
+
+    // Get current position of the mouse in terms of WebGL.
+    var currX = 2 * e.clientX / canvas.width - 1;
+    var currY = 2 * ( canvas.height - e.clientY ) / canvas.height - 1;
+}
+
+function mouseMove( e )
+{
+    if ( !drag )
+    {
+        return;
+    }
+
+    newX = 2 * e.clientX / canvas.width - 1;
+    newY = 2 * ( canvas.height - e.clientY ) / canvas.height - 1;
+
+    for ( var i = 0; i < selectedCards.length; ++i )
+    {
+        cardVertices[ selectedCards[ i ] ][ 0 ] = newX - card_dim.width / 2;
+        cardVertices[ selectedCards[ i ] ][ 1 ] = newY - ( card_dim.offset * i );
+    }
+}
+
+// Init cards. 
+// Generate a "deck" with the values 0 - 51 in random order.
+// "deal" the cards in specified places ( the rows );
+function init()
+{
+    // Init stuffs
+    init_deck();
+    init_cards();
+    init_render();
+
+    card_info.face_up = []; 
+    card_info.place_pos = []; 
+    card_info.click_pos = []; 
+}
+
+function init_deck()
+{
+    deck = [ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 
+            13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 
+            26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 
+            39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51 ];
+
+    // Shuffle the deck - Modern Fisher-Yates Shuffle Algorithm O( n )
+    //      unless Math.random() has a problem.
+    // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+    for ( var i = 51; i > 0; --i )
+    {
+        var index = Math.floor( Math.random() * i );
+        var tmp = deck[ i ];
+        deck[ i ] = deck[ index ];
+        deck[ index ] = tmp;
+    }
+}
+
+function init_cards()
+{
+    // Initalize stacks. 7 stacks with 1-2-3-4-5-6-7 cards.
+    for ( var i = 0; i < 7; ++i )
+    {
+        for ( var k = 0; k < i + 1; ++k )
+        {
+            stack_info.stacks[ i ].push( deck.pop() );
+        }
+    }
+
+    // Init the card positions in the deck's initial position.
+    for ( var i = 0; i < 52; ++i )
+    {
+        cards.cards_x[ i ] = -card_dim.offset;
+        cards.cards_y[ i ] = card_dim.offset;
+    }
+
+    // Initialize each card's position in the stacks.
+    var x_off = -card_dim.offset + card_dim.width * i;
+    for ( var i = 0; i < 7; ++i )
+    {
+        var stack = stack_info.stacks[ i ];
+        for ( var k = 0; k < i + 1; ++k )
+        {
+            cards.cards_x[ stack[ k ] ] = x_off;
+            cards.cards_y[ stack[ k ] ] = 0.481 - ( card_dim.offset * k );
+        }
+    }
+}
+
+function init_render()
+{
+    render_info.render_vertices_x = []; 
+    render_info.render_vertices_y = []; 
+    render_info.render_colors = [];
+}
+
+window.onload = function()
 {
     canvas = document.getElementById( "gl-canvas" );
 
@@ -96,128 +248,4 @@ window.onload = function init()
     init();
 
     setInterval( render, 15 );
-
-    function render()
-    {
-        gl.clear( gl.COLOR_BUFFER_BIT );
-
-        // Push card positions to vertex shader buffer.
-        for ( var i = 0; i < renderOrder.length; ++i )
-        {
-            gl.bindBuffer( gl.ARRAY_BUFFER, verticesBuffer );
-            var tmp = [
-                cardVertices[ renderOrder[ i ] ],
-                vec2( cardVertices[ renderOrder[ i ] ][ 0 ] + CARD_WIDTH, 
-                        cardVertices[ renderOrder[ i ] ][ 1 ] ),
-                vec2( cardVertices[ renderOrder[ i ] ][ 0 ], 
-                        cardVertices[ renderOrder[ i ] ][ 1 ] - CARD_HEIGHT ),
-                vec2( cardVertices[ renderOrder[ i ] ][ 0 ] + CARD_WIDTH,
-                        cardVertices[ renderOrder[ i ] ][ 1 ] - CARD_HEIGHT ),
-                vec2( cardVertices[ renderOrder[ i ] ][ 0 ], 
-                        cardVertices[ renderOrder[ i ] ][ 1 ] - CARD_HEIGHT ),
-                vec2( cardVertices[ renderOrder[ i ] ][ 0 ] + CARD_WIDTH, 
-                        cardVertices[ index ][ 1 ] )
-            ];
-            gl.bufferSubData( gl.ARRAY_BUFFER, 48 * i, flatten( tmp ) );
-        }
-
-        gl.drawArrays( gl.TRIANGLES, 0, 312 );
-    }
-
-    function mouseDown( e )
-    {
-        // Tell the program that the mouse is pressing down.
-        drag = true;
-        
-        // Get current position of the mouse in terms of WebGL.
-        var currX = 2 * e.clientX / canvas.width - 1;
-        var currY = 2 * ( canvas.height - e.clientY ) / canvas.height - 1;
-    }
-
-    //NOTE: 0 - 6 are field stacks, 7 is draw deck, 8 - 11 are the finish stacks
-    function mouseUp( e )
-    {
-        drag = false;
-
-        // Get current position of the mouse in terms of WebGL.
-        var currX = 2 * e.clientX / canvas.width - 1;
-        var currY = 2 * ( canvas.height - e.clientY ) / canvas.height - 1;
-    }
-
-    function mouseMove( e )
-    {
-        if ( !drag )
-        {
-            return;
-        }
-
-        newX = 2 * e.clientX / canvas.width - 1;
-        newY = 2 * ( canvas.height - e.clientY ) / canvas.height - 1;
-
-        for ( var i = 0; i < selectedCards.length; ++i )
-        {
-            cardVertices[ selectedCards[ i ] ][ 0 ] = newX - CARD_WIDTH / 2;
-            cardVertices[ selectedCards[ i ] ][ 1 ] = newY - ( offset * i );
-        }
-    }
-
-    // Init cards. 
-    // Generate a "deck" with the values 0 - 51 in random order.
-    // "deal" the cards in specified places ( the rows );
-    function init()
-    {
-        // Init stuffs
-        deck = [ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 
-                13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 
-                26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 
-                39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51 ];
-
-        // Shuffle the deck - Modern Fisher-Yates Shuffle Algorithm O( n )
-        //      unless Math.random() has a problem.
-        // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
-        for ( var i = 51; i > 0; --i )
-        {
-            var index = Math.floor( Math.random() * i );
-            var tmp = deck[ i ];
-            deck[ i ] = deck[ index ];
-            deck[ index ] = tmp;
-        }
-
-        // Initalize stacks. 7 stacks with 1-2-3-4-5-6-7 cards.
-        for ( var i = 0; i < 7; ++i )
-        {
-            var stack = stacks[ i ];
-            for ( var k = 0; k < i + 1; ++k )
-            {
-                stacks[ i ].push( deck.pop() );
-            }
-        }
-
-        // Init the card positions in the deck's initial position.
-        for ( var i = 0; i < 52; ++i )
-        {
-            card_x[ i ] = -offset;
-            card_y[ i ] = offset;
-        }
-
-        // Initialize each card's position in the stacks.
-        for ( var i = 0; i < 7; ++i )
-        {
-            var stack = stacks[ i ];
-            var x_off = -offset + CARD_WIDTH * i;
-            for ( var k = 0; k < i + 1; ++k )
-            {
-                var indx = stack[ k ];
-                card_x[ indx ] = x_off;
-                card_y[ indx ] = 0.481 - offset * k;
-            }
-        }
-
-        // edit in order:
-        render_card = []; 
-        render_vertices = []; 
-        face_up = []; 
-        place_pos = []; 
-        click_pos = []; 
-    }
 };
